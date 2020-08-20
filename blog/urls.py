a18a6c3d3181704786888d62b2a24a8bf7a70121
Ali Hassan <alih.qureshi@hotmail.com>
2020-08-20 19:26:08 +0500

    Revert "Added following features"
    This reverts commit 50a9ba3c36d1f2d96a7147fb64e00feea17c605a.

--- blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('blog/', views.home, name='blog-home'),
-    path('about/', views.about, name='blog-about')
- ]
