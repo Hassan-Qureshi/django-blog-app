0410c02746481c111b726ae1589d5c82cca1b1ed
Ali Hassan <alih.qureshi@hotmail.com>
2020-08-23 01:50:15 +0500

    - Fixed Nav bar issue of getting user once user logged in - Redirected user to login page after registration - Added the logout url and fixed the issue

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='sign-up'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='log-in'),
-    path('logout/', auth_views.LoginView.as_view(template_name='users/logout.html'), name='log-out'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='log-out'),
 ]
