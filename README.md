3a7c341cc899259ce3f04dd1c22e50e5940100bc
Ali Hassan <alih.qureshi@hotmail.com>
2020-09-08 01:07:55 +0500

    - Added profile photo module - Updated the webiste for user profile picture - Added pillow library in requirements.txt - Set default photo - Removed migrations files - Updated the Readme.md file for clear instructions

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-####### Pre Requisites 
+## Getting started
+ 
+#### 1. Clone Project
+``` bash
+git clone https://github.com/Hassan-Qureshi/django-blog-app.git
 ```
-
+#### 2. Create a Virtual Environment
+``` bash
+pip install virtualenv
+virtualenv <ENV-NAME>
+./<ENV-NAME>/Scripts/activate
+```
+Replace **<ENV-NAME>** with virtual environment name you want to give.
+#### 3. Install Dependencies 
+``` bash
+pip install -r requirements.txt
+```
+#### 4. Create Migrations 
+``` bash
+python manage.py makemigrations
+python manage.py migrate
+```
+#### 5. Run the Project 
+``` bash
+python manage.py runserver
 ```
--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,9 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='sign-up'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='log-in'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='log-out'),
+    path('auth/register/', views.register, name='sign-up'),
+    path('auth/login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='log-in'),
+    path('auth/logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='log-out'),
+    path('profile/', views.profile, name='profile'),
+
 ]
