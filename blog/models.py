52f796493f7d1f1de6283a02db094b86231d030f
Ali Hassan <alih.qureshi@hotmail.com>
2020-09-20 03:21:43 +0500

    Added CRUD functionality for blog

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 
 # Create your models here.
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(to=User, on_delete=models.CASCADE)
     date_posted = models.DateTimeField(default=timezone.now)
     content = models.TextField()
+
+    def get_absolute_url(self):
+        return reverse(viewname='post-detail', kwargs={'pk': self.pk})
+
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView,
     DetailView,
-    CreateView
+    CreateView,
+    UpdateView,
+    DeleteView
 )
 
 
 def home(request):
     context = {
-            'data': Post.objects.all()
-        }
+        'data': Post.objects.all()
+    }
     return render(request, 'blog/home.html', context=context)
 
 
@@ -32,9 +35,53 @@ class PostDetailView(DetailView):
     # we will not be setting context_object_name
 
 
-class PostCreateView(CreateView):
+# Using `LoginRequiredMixin` because we want to validate if the user is logged in or not.
+class PostCreateView(LoginRequiredMixin, CreateView):
+    # We need to create the post so there will be a form for this we only need fields which should be in the form
     model = Post
     fields = ['title', 'content']
+    template_name = 'blog/post_create.html'
+
+    def form_valid(self, form):
+        """
+        Overriding form method because there should be a user who create the post and
+        that user should be logged in user.
+        """
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_create.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    # In order to stop other users to edit your post we have to extend class-based view to UserPassesTestMixin
+    # and override the following method
+
+    def test_func(self):
+        # Get the post being updated
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    # success_url is required because once post is deleted it will automatically redirected towards home page
+    success_url = '/'
+
+    def test_func(self):
+        # Get the post being updated
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
@@ -42,4 +89,5 @@ def about(request):
 
 
 def welcome(request):
-    return HttpResponse('Welcome page')
+    if request:
+        return HttpResponse('Welcome page')
